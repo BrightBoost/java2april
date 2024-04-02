52a76e5ef4e3b1f4161c5da64a5666592a706576
BrightBoost <info@brightboost.nl>
2024-04-02 16:00:08 +0200

    oop

--- src/oop/Car.java
+++ src/oop/Car.java
@@ -1,7 +1,13 @@
 package oop;
 
-public class Car extends Vehicle {
+public class Car extends Vehicle implements Chargeable {
     public void honk() {
         System.out.println("honk!");
     }
+
+    @Override
+    public void charge() {
+        System.out.println("charging car");
+    }
+    Object
 }
